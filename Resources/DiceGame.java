b28ac9cba540278dc2d5e22e52142954a34c17b3
Brittany Pruneau <69804752+BrittanyPruneau@users.noreply.github.com>
2021-08-21 14:25:43 -0600

    Add files via upload

---
+++ Resources/DiceGame.java
@@ -0,0 +1,48 @@
+package guiImage.Resources;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+
+public class DiceGame extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DiceGame frame = new DiceGame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public DiceGame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JButton btnNewButton = new JButton("Dice");
+		btnNewButton.setIcon(new ImageIcon(DiceGame.class.getResource("/guiImage/Resources/download.jpg")));
+		contentPane.add(btnNewButton, BorderLayout.CENTER);
+	}
+
+}
